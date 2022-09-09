833eb1d8b49873b22006d21a8b610b6689bca320
kaique <kaiquesilva49@hotmail.com>
2022-09-09 20:58:14 -0300

    estilizando registro de usuario

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -4,6 +4,7 @@ import { save } from "../../api/user";
 import Button from "../Button";
 import InputPassword from "../InputPassword";
 import InputText from "../InputText";
+import "./Register.css";
 
 const Register = () => {
   const [name, setName] = useState("");
@@ -18,27 +19,30 @@ const Register = () => {
     navigate("/");
   };
   return (
-    <div>
-      <InputText
-        label="Nome"
-        value={name}
-        setValue={setName}
-        placeholder="Digite seu nome"
-      />
-      <InputText
-        label="E-mail"
-        value={email}
-        setValue={setEmail}
-        placeholder="Digite seu e-mail"
-      />
-      <InputPassword
-        label="Senha"
-        value={password}
-        setValue={setPassword}
-        type="password"
-        placeholder="Digite sua senha"
-      />
-      <Button onClick={saveUser} title="salvar" />
+    <div className="register-form">
+      <h1>Crie sua conta</h1>
+      <div className="register-form-inputs">
+        <InputText
+          label="Nome"
+          value={name}
+          setValue={setName}
+          placeholder="Digite seu nome"
+        />
+        <InputText
+          label="E-mail"
+          value={email}
+          setValue={setEmail}
+          placeholder="Digite seu e-mail"
+        />
+        <InputPassword
+          label="Senha"
+          value={password}
+          setValue={setPassword}
+          type="password"
+          placeholder="Digite sua senha"
+        />
+      </div>
+      <Button className="btn" onClick={saveUser} title="salvar" />
     </div>
   );
 };
