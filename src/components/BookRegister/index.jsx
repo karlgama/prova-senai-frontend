934be20e78d3e7548b530a60ae3c34c47b671063
kaique <kaiquesilva49@hotmail.com>
2022-09-10 22:56:35 -0300

    iniciando upload de imagens

--- src/components/BookRegister/index.jsx
+++ src/components/BookRegister/index.jsx
@@ -4,22 +4,53 @@ import { useState } from "react";
 import Button from "../Button";
 import { save } from "../../api/books";
 import "./BookRegister.css";
+import Dropzone from "react-dropzone";
+import { DropContainer, UploadMessage } from "./styles";
+import ImagePreview from "../ImagePreview";
 
 const BookRegister = (props) => {
-  const [imgPeq, setImgPeq] = useState("");
-  const [imgGrd, setImgGrd] = useState("");
+  // const [imgPeq, setImgPeq] = useState(null);
+  // const [imgGrd, setImgGrd] = useState(null);
+  const [uploadFiles, setUploadFiles] = useState([]);
   const [titulo, setTitulo] = useState("");
   const [preco, setPreco] = useState();
   const [detalhes, setDetalhes] = useState("");
-
+  const [previews, setPreviews] = useState([]);
   const navigate = useNavigate();
 
+  const handleUpload = async (files) => {
+    let images = [];
+    if (files[0]) {
+      const imgP = {
+        img: files[0],
+        preview: URL.createObjectURL(files[0]),
+      };
+      images.push(imgP);
+    }
+    if (files[1]) {
+      const imgP = {
+        img: files[1],
+        preview: URL.createObjectURL(files[1]),
+      };
+      images.push(imgP);
+    }
+    setUploadFiles([files[0], files[1]]);
+    setPreviews(images);
+  };
+
   const saveBook = async () => {
-    const book = { imgPeq, imgGrd, titulo, preco, detalhes };
+    const book = { uploadFiles, titulo, preco, detalhes };
+    console.log("salvar", book);
     await save(book);
     navigate("/books");
   };
-
+  const renderDragMessaget = (isDragActive, isDragReject) => {
+    if (!isDragActive)
+      return <UploadMessage>Arraste arquivos aqui...</UploadMessage>;
+    if (isDragReject)
+      return <UploadMessage type="error">Arquivo não suportado</UploadMessage>;
+    return <UploadMessage type="success">Solte os arquivos aqui</UploadMessage>;
+  };
   return (
     <div className="book-register">
       <h1 className="title">Registre um novo livro</h1>
@@ -43,19 +74,25 @@ const BookRegister = (props) => {
           setValue={setPreco}
           placeholder="Digite o preço do livro"
         />
+        <Dropzone accept="image/*" onDropAccepted={handleUpload}>
+          {({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
+            <DropContainer
+              {...getRootProps()}
+              isDragActive={isDragActive}
+              isDragReject={isDragReject}
+            >
+              <input {...getInputProps()} />
+              {renderDragMessaget(isDragActive, isDragReject)}
+            </DropContainer>
+          )}
+        </Dropzone>
+        <div className="images-preview">
+          {previews.map((preview) => (
+            <ImagePreview src={preview.preview} />
+          ))}
+        </div>
       </div>
-      {/* <InputText
-        label="Nome"
-        value={name}
-        setValue={setName}
-        placeholder="Digite seu nome"
-      />{" "}
-      <InputText
-        label="Nome"
-        value={name}
-        setValue={setName}
-        placeholder="Digite seu nome"
-      /> */}
+
       <Button className="btn" onClick={saveBook} title="salvar" />
     </div>
   );
